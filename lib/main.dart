fd8c1dacab741d3f87691681ecc61209d285f799
Abid <abidroid1@gmail.com>
2025-05-02 11:30:46 +0500

    dynamic grideview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uoajk_day3/screens/dynamic_gridview_screen.dart';
 import 'package:uoajk_day3/screens/dynamic_listview_screen.dart';
 import 'package:uoajk_day3/screens/gridview_demo_screen.dart';
 import 'package:uoajk_day3/screens/listview_demo_screen.dart';
@@ -13,6 +14,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
@@ -20,7 +23,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const GridviewDemoScreen(),
+      home: const DynamicGridviewScreen(),
     );
   }
 }
